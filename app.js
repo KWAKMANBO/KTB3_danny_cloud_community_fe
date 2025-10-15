0822e3eadd356c955584cb500da7754aa6a8fd04
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-10-15 15:25:14 +0900

    header 컴포넌트 추가

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000에서 서버 실행 중 ")
+});
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src','page', 'login.html'));
+})
---
+++ src/page/component/headerComponent.js
@@ -0,0 +1,3 @@
+
+const header= document.querySelector('header')
+header.innerHTML = '<h1>아무 말 대잔치</h1>';
